71f3e6cac66a1fdc2ad77271e012a30f99cff312
Mostafa-Ehab <mostafa.e.h.m.r@gmail.com>
2024-08-01 19:38:35 +0300

    Fix missing import

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, JsonResponse, Http404
 from django.urls import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
